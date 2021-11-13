8aa8d8eade731d567a4278334c9443a4d976e1ef
alirezaproject <shekoohianproject@gmail.com>
2021-11-13 15:00:22 +0330

    Fix Site Template

---
+++ EndPoint/ViewComponents/SiteViewComponent.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.ViewComponents
+{
+    public class HomeSectionViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("HomeSection");
+        }
+    }
+
+    public class AboutSectionViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("AboutSection");
+        }
+    }
+
+    public class ResumeSectionViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("ResumeSection");
+        }
+    }
+    public class PortfolioSectionViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("PortfolioSection");
+        }
+    }
+
+    public class ContactSectionViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("ContactSection");
+        }
+    }
+}
